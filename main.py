567db378af9fc37f73996d334daff11549f3fce7
Azathoth <imperialbenjie393@gmail.com>
2026-03-05 19:42:26 +0800

    registered chatbot ai router

--- main.py
+++ main.py
@@ -58,7 +58,9 @@ from routers.v1.project_router import router as project_router
 from routers.v1.project_invitation_router import router as project_invitation_router
 from routers.v1.project_member_router import router as project_member_router
 from routers.v1.chatbot_api_key_router import router as chatbot_api_key_router
+from routers.v1.chat_router import router as chat_ai_router
 app.include_router(project_router)
 app.include_router(project_invitation_router)
 app.include_router(project_member_router)
 app.include_router(chatbot_api_key_router)
+app.include_router(chat_ai_router)
