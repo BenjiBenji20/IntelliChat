f066a6e2d241f26263e961c43a9c42289ee250d1
Azathoth <imperialbenjie393@gmail.com>
2026-03-12 12:39:43 +0800

    added new env vars as nullable

--- configs/settings.py
+++ configs/settings.py
@@ -7,29 +7,46 @@ class Settings(BaseSettings):
     DEBUG: bool = True
     ENV: Literal["dev", "prod", "test"] = "dev"
     
-    CORS_DEV_ORIGIN: str | None = None
-    CORS_PROD_ORIGIN: str | None = None
+    CORS_DEV_ORIGIN: str = None
+    CORS_PROD_ORIGIN: str = None
     
     # db settings
     # temporarily nullable for successfully deployment in GCP cloud run 
-    POSTGRES_USER: str | None = None
-    POSTGRES_PASSWORD: SecretStr | None = None
-    POSTGRES_HOST: str | None = None
-    POSTGRES_PORT: int | None = None
-    POSTGRES_DATABASE: str | None = None
+    POSTGRES_USER: str = None
+    POSTGRES_PASSWORD: SecretStr = None
+    POSTGRES_HOST: str = None
+    POSTGRES_PORT: int = None
+    POSTGRES_DATABASE: str = None
     
     # api secret key security
-    API_KEY_HEADER_NAME: str | None = "Anonymous"
+    API_KEY_HEADER_NAME: str = "Anonymous"
     
-    SECRET_KEY_PREFIX: str | None = None
-    ENCRYPTION_KEY: str | None = None
+    SECRET_KEY_PREFIX: str = None
+    ENCRYPTION_KEY: str = None
     
-    UPSTASH_REDIS_URL: str | None = None
-    UPSTASH_REDIS_TOKEN: str | None = None
+    UPSTASH_REDIS_URL: str = None
+    UPSTASH_REDIS_TOKEN: str = None
     
     # AI api
-    LLM_API_KEY: str | None = None
-    LLM_NAME: str | None = None
+    LLM_API_KEY: str = None
+    LLM_NAME: str = None
+    
+    # GOOGLE CLOUD STORAGE SERVICE KEY
+    TYPE: str = None 
+    PROJECT_ID: str = None 
+    PRIVATE_KEY_ID: str = None 
+    PRIVATE_KEY: str = None 
+    CLIENT_EMAIL: str = None 
+    CLIENT_ID: str = None 
+    AUTH_URI: str = None 
+    TOKEN_URI: str = None 
+    AUTH_PROVIDER_X509_CERT_URL: str = None 
+    CLIENT_X509_CERT_URL: str = None 
+    UNIVERSE_DOMAIN: str = None
+    
+    GCS_BUCKET_NAME: str = None
+    GCS_UPLOAD_URL_EXPIRY_SECONDS: int = 900
+    GCS_DOWNLOAD_URL_EXPIRY_SECONDS: int = 3600
     
     class Config:
         env_file = ".env"
@@ -44,5 +61,22 @@ class Settings(BaseSettings):
             f"{self.POSTGRES_PORT}/"
             f"{self.POSTGRES_DATABASE}?ssl=require"
         )
+        
+    @property
+    def get_gcs_credentials(self) -> dict:
+        """Returns the Google credentials as a dictionary"""
+        return {
+            "type": self.TYPE,
+            "project_id": self.PROJECT_ID,
+            "private_key_id": self.PRIVATE_KEY_ID,
+            "private_key": self.PRIVATE_KEY,
+            "client_email": self.CLIENT_EMAIL,
+            "client_id": self.CLIENT_ID,
+            "auth_uri": self.AUTH_URI,
+            "token_uri": self.TOKEN_URI,
+            "auth_provider_x509_cert_url": self.AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": self.CLIENT_X509_CERT_URL,
+            "universe_domain": self.UNIVERSE_DOMAIN
+        }
     
 settings = Settings()
