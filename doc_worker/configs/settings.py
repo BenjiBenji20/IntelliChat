dbeda664ab5da8a9ccc63f782ea766bf19748737
Azathoth <imperialbenjie393@gmail.com>
2026-03-14 10:02:15 +0800

    added env vars

--- doc_worker/configs/settings.py
+++ doc_worker/configs/settings.py
@@ -14,8 +14,33 @@ class Settings(BaseSettings):
     LLM_API_KEY: str = None
     LLM_NAME: str = None
     
+    # GCP
+    CLOUD_TASK_QUEUE_PATH: str = None
+    WORKER_URL: str = None
+    GCP_PROJECT_ID: str = None
+    GCP_REGION: str = None
+    GCS_BUCKET_NAME: str = None
+    MAIN_API_SERVICE_ACC_EMAIL: str = None
+
+    # Supabase
+    POSTGRES_USER: str = None
+    POSTGRES_PASSWORD: SecretStr = None
+    POSTGRES_HOST: str = None
+    POSTGRES_PORT: str = None
+    POSTGRES_DATABASE: str = None
+    
     class Config:
         env_file = "doc_worker.env"
         extra = "ignore"
+        
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql://{self.POSTGRES_USER}:"
+            f"{self.POSTGRES_PASSWORD.get_secret_value()}"
+            f"@{self.POSTGRES_HOST}:"
+            f"{self.POSTGRES_PORT}/"
+            f"{self.POSTGRES_DATABASE}?ssl=require"
+        )
     
 settings = Settings()
