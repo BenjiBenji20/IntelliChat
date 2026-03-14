4d48ca35d13c50bf3816c51f402add621cf42083
Azathoth <imperialbenjie393@gmail.com>
2026-03-14 10:02:59 +0800

    build: establish async postgres connection in lifespan

--- main_doc_worker.py
+++ main_doc_worker.py
@@ -9,12 +9,31 @@ logger.debug(f"Starting app - PORT from env: {os.environ.get('PORT', 'not set')}
 logger.debug(f"Binding to 0.0.0.0:{os.environ.get('PORT', '8080')}")
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
 from doc_worker.configs.settings import settings
+from doc_worker.db import db_session
+from doc_worker.db.db_session import init_db_pool, close_db_pool
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        await init_db_pool()
+        await db_session.pool.fetchrow("SELECT 1") # try fetch to trigger cold start
+        print("\n\nPostgres connected successfully!")
+        
+        yield 
+        
+    finally:
+        await close_db_pool()
+        print("\n\nPostgres acyncpg engine disposed...")
+        print("Application shutdown...")
+
 
 app = FastAPI(
-    title=settings.APP_NAME
+    title=settings.APP_NAME,
+    lifespan=lifespan
 )
 
 app.add_middleware(
