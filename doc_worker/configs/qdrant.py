3be469a2bb216fe39ea459b353d4a92b86dad770
Azathoth <imperialbenjie393@gmail.com>
2026-03-15 21:13:27 +0800

    build: async qdrant client

---
+++ doc_worker/configs/qdrant.py
@@ -0,0 +1,19 @@
+from qdrant_client import AsyncQdrantClient
+from doc_worker.configs.settings import settings
+
+_qdrant_client: AsyncQdrantClient = None
+
+async def init_qdrant_client():
+    global _qdrant_client
+    _qdrant_client = AsyncQdrantClient(
+        url=settings.QDRANT_CLUSTER_ENDPOINT,
+        api_key=settings.QDRANT_API_KEY,
+    )
+
+async def close_qdrant_client():
+    global _qdrant_client
+    if _qdrant_client:
+        await _qdrant_client.close()
+
+def get_qdrant_client() -> AsyncQdrantClient:
+    return _qdrant_client
