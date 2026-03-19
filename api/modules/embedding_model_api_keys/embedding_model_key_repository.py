a5d5a37140829bc5b996e26f7357e6514156b1fc
Azathoth <imperialbenjie393@gmail.com>
2026-03-19 10:17:42 +0800

    feat: fetch embedding model keys rows

--- api/modules/embedding_model_api_keys/embedding_model_key_repository.py
+++ api/modules/embedding_model_api_keys/embedding_model_key_repository.py
@@ -1,7 +1,12 @@
+from uuid import UUID
+
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from api.base.base_crud_repository import BaseCrudRepository
 from api.models.embedding_model_key import EmbeddingModelKey
+from api.models.chatbot import Chatbot
 
 
 class EmbeddingModelKeyRepository(BaseCrudRepository[EmbeddingModelKey]):
@@ -12,4 +17,37 @@ class EmbeddingModelKeyRepository(BaseCrudRepository[EmbeddingModelKey]):
     """
     def __init__(self, db: AsyncSession) -> None:
         super().__init__(EmbeddingModelKey, db)
+        
+        
+    async def get_embedding_model_details(self, chatbot_id: UUID) -> dict:
+        """
+        Fetch model details necessary for retrieval.
+        """
+        try:
+            
+            stmt = (
+                select(
+                    EmbeddingModelKey.id,
+                    EmbeddingModelKey.api_key_encrypted,
+                    EmbeddingModelKey.embedding_model_name,
+                    EmbeddingModelKey.provider
+                )
+                .join(
+                    Chatbot, Chatbot.id == EmbeddingModelKey.chatbot_id
+                )
+                .where(
+                    Chatbot.id == chatbot_id
+                )
+            )
+            
+            result = await self.db.execute(stmt)
+            
+            return result.mappings().first()
+            
+        except HTTPException:
+            raise
+        
+        except Exception as e:
+            await self.db.rollback()
+            raise e
         
