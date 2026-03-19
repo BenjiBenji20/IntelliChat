d5f7eed7ec811ae2e1d81a783c10b90ad99e2022
Azathoth <imperialbenjie393@gmail.com>
2026-03-19 10:18:30 +0800

    feat: added qdrant client in life_span for startup and register retrieval router

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ from api.models.project_member import ProjectMember
 from api.models.project_invitation import ProjectInvitation
 from api.models.chatbot_behavior import ChatbotBehavior
 
+from api.configs.qdrant import init_qdrant_client
+
 @asynccontextmanager
 async def life_span(app: FastAPI):
     try:
@@ -41,6 +43,9 @@ async def life_span(app: FastAPI):
             # await conn.run_sync(Base.metadata.create_all)
             await conn.execute(text("SELECT 1"))
             print("\n\nPostgres connected successfully!")
+            
+            await init_qdrant_client()
+            print("\n\nQDrant connected successfully!")
             
         yield
         
@@ -72,6 +77,7 @@ from api.routers.v1.chatbot_api_key_router import router as chatbot_api_key_rout
 from api.routers.v1.chatbot_router import router as chat_ai_router
 from api.routers.v1.behavior_studio_router import router as behavior_studio_router
 from api.routers.v1.document_router import router as document_router
+from api.routers.v1.retrieval_router import router as retrieval_router
 app.include_router(project_router)
 app.include_router(project_invitation_router)
 app.include_router(project_member_router)
@@ -79,3 +85,4 @@ app.include_router(chatbot_api_key_router)
 app.include_router(chat_ai_router)
 app.include_router(behavior_studio_router)
 app.include_router(document_router)
+app.include_router(retrieval_router)
