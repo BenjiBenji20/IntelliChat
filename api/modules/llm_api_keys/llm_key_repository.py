4fd30508d61eeb20d00bbfeb9e255b550a9ca88f
Azathoth <imperialbenjie393@gmail.com>
2026-03-20 22:52:42 +0800

    feat: fetch llm rows usefull for chat ai

--- api/modules/llm_api_keys/llm_key_repository.py
+++ api/modules/llm_api_keys/llm_key_repository.py
@@ -1,6 +1,11 @@
+from uuid import UUID
+
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from api.base.base_crud_repository import BaseCrudRepository
+from api.models.chatbot import Chatbot
 from api.models.llm_key import LlmKey
 
 
@@ -12,4 +17,28 @@ class LlmKeyRepository(BaseCrudRepository[LlmKey]):
     """
     def __init__(self, db: AsyncSession) -> None:
         super().__init__(LlmKey, db)
+    
+    
+    async def get_llm_details(self, chatbot_id: UUID) -> dict | None:
+        try:
+            stmt = (
+                select(
+                    LlmKey.api_key_encrypted,
+                    LlmKey.llm_name,
+                    LlmKey.provider.label("llm_provider"),
+                    LlmKey.temperature
+                )
+                .join(Chatbot, Chatbot.id == LlmKey.chatbot_id)
+                .where(Chatbot.id == chatbot_id)
+            )
+            
+            result = await self.db.execute(stmt)
+            return result.mappings().one_or_none()
+                        
+        except HTTPException:
+            raise
         
+        except Exception:
+            self.db.rollback()
+            raise
+    
